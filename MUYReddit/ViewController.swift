cccb3c1ace496d0485b9cdc12f9c6e74d3b0880f
Nico Ameghino <nicolas.ameghino@gmail.com>
2016-09-06 23:40:24 -0300

    Initial Commit

---
+++ MUYReddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MUYReddit
+//
+//  Created by Nicolas Ameghino on 9/6/16.
+//  Copyright © 2016 Nicolas Ameghino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
