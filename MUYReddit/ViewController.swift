aec045ba4ef81ad3e88c17d1785b77d2a5463e7f
Nico Ameghino <nicolas.ameghino@gmail.com>
2016-10-02 15:45:23 -0300

    Change attribution string to spanish Add a constructor for the view model that takes the subreddit as a parameter Add support for continue token Add PostListSection enum Change CellIdentifier constant to support two cell types (post and loading) Remove -update call from viewWillAppear, now relying on setting the view model to trigger load Add a tap gesture recognizer to the navigation bar to show subreddit change screen Add handling for the new `loading' cell type

--- MUYReddit/ViewController.swift
+++ MUYReddit/ViewController.swift
@@ -11,31 +11,31 @@ import UIKit
 class RedditPostViewModel : NSObject {
     private static let backgroundQueue = DispatchQueue(label: "viewmodel")
     private let post: RedditPost
-    
+
     init(post: RedditPost) {
         self.post = post
     }
-    
+
     var title: String { return post.title }
     var subtitle: NSAttributedString {
         let subtitle = NSMutableAttributedString()
-        let attribution = NSAttributedString(string: "by \(post.author)", attributes: [NSForegroundColorAttributeName : UIColor.black])
+        let attribution = NSAttributedString(string: "por \(post.author)", attributes: [NSForegroundColorAttributeName : UIColor.black])
         let source = NSAttributedString(string: " - via \(post.domain)", attributes: [NSForegroundColorAttributeName : UIColor.gray])
         subtitle.append(attribution)
         subtitle.append(source)
         return subtitle
     }
-    
+
     private var cachedResult: Result<UIImage>? = nil
     private var inflightThumbnailTask: DispatchWorkItem? = nil
-    
+
     func fetchThumbnail(callback: @escaping (Result<UIImage>) -> Void) {
-        
+
         if let result = cachedResult {
             callback(result)
             return
         }
-        
+
         inflightThumbnailTask = DispatchWorkItem(qos: .userInitiated, flags: []) { [weak self] in
             guard let sself = self else { return }
             do {
@@ -60,7 +60,7 @@ class RedditPostViewModel : NSObject {
         }
         RedditPostViewModel.backgroundQueue.async(execute: inflightThumbnailTask!)
     }
-    
+
     func cancelThumbnailFetch() {
         inflightThumbnailTask?.cancel()
     }
@@ -71,37 +71,52 @@ class RedditPostListViewModel : NSObject {
 
     var subreddit: String = "argentina"
     var count: Int { return posts.count }
-    
+
     private let service = RedditAPI()
-    
+
+    private var continueToken: String? = nil
+
+    init(subreddit: String) {
+        self.subreddit = subreddit
+    }
+
     func fetch(callback: @escaping (Void) -> Void) {
-        _ = service.fetchListingFor(subreddit: subreddit) { [weak self] result in
+        _ = service.fetchListingFor(subreddit: subreddit, continueToken: continueToken) { [weak self] result in
             defer { callback() }
             guard let sself = self else { return }
             switch result {
             case .error(let error):
                 fatalError("\(error)")
             case .success(let data):
-                sself.posts = data.posts.map { RedditPostViewModel(post: $0) }
+                sself.continueToken = data.continueToken
+                sself.posts += data.posts.map { RedditPostViewModel(post: $0) }
             }
         }
     }
-    
+
     subscript(i: Int) -> RedditPostViewModel {
         return posts[i]
     }
 }
 
+private enum PostListSection: Int {
+    case posts
+    case loadMore
+
+    static let NumberOfSections = 2
+}
+
 class RedditPostListViewController: UIViewController {
-    
-    fileprivate static let ReuseIdentifier = "RedditPostCell"
-    
-    var postList: RedditPostListViewModel = RedditPostListViewModel() {
+
+    fileprivate static let PostCellReuseIdentifier = "RedditPostCell"
+    fileprivate static let PagingCellReuseIdentifier = "PagingCell"
+
+    var postList: RedditPostListViewModel = RedditPostListViewModel(subreddit: "argentina") {
         didSet {
             update()
         }
     }
-    
+
     @IBOutlet weak var tableView: UITableView! {
         didSet {
             tableView.dataSource = self
@@ -110,18 +125,28 @@ class RedditPostListViewController: UIViewController {
             tableView.rowHeight = UITableViewAutomaticDimension
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
+    override func viewWillAppear(_ animated: Bool) {
+        navigationItem.title = "Cargando..."
+
+        let tgr = UITapGestureRecognizer(target: self, action: #selector(showSubredditSelector))
+        tgr.numberOfTapsRequired = 1
+        tgr.numberOfTouchesRequired = 1
+
+        navigationController?.navigationBar.addGestureRecognizer(tgr)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.navigationBar.gestureRecognizers?.forEach { navigationItem.titleView?.removeGestureRecognizer($0) }
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        update()
     }
 }
 
@@ -133,33 +158,84 @@ extension RedditPostListViewController {
         }
     }
 
+    @objc
+    fileprivate func showSubredditSelector() {
+        let alertController = UIAlertController(title: "Subreddit?", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        alertController.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+
+        let acceptAction = UIAlertAction(title: "Aceptar", style: .default) { [weak self] _ in
+            guard let sself = self else { return }
+            guard let text = alertController.textFields?.first?.text else { fatalError("alert controller has no textfields!") }
+
+            let subreddit: String = {
+                if text.hasPrefix("r/"), let subreddit = text.components(separatedBy: "r/").last {
+                    return subreddit
+                } else {
+                    return text
+                }
+            }()
+
+            sself.postList = RedditPostListViewModel(subreddit: subreddit)
+        }
+
+        alertController.addAction(acceptAction)
+
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
 extension RedditPostListViewController : UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return PostListSection.NumberOfSections
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return postList.count
+        guard let section = PostListSection(rawValue: section) else { fatalError("unknown section!") }
+        switch section {
+        case .posts:
+            return postList.count
+        case .loadMore:
+            return 1
+        }
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: RedditPostListViewController.ReuseIdentifier, for: indexPath)
-        cell.selectionStyle = .none
-        let vm = postList[indexPath.row]
-        set(content: vm, forCell: cell)
-        return cell
+        guard let section = PostListSection(rawValue: indexPath.section) else { fatalError("unknown section!") }
+        switch section {
+        case .posts:
+            let cell = tableView.dequeueReusableCell(withIdentifier: RedditPostListViewController.PostCellReuseIdentifier, for: indexPath)
+            cell.selectionStyle = .none
+            let vm = postList[indexPath.row]
+            set(content: vm, forCell: cell)
+            return cell
+        case .loadMore:
+            let cell = tableView.dequeueReusableCell(withIdentifier: RedditPostListViewController.PagingCellReuseIdentifier, for: indexPath)
+            cell.selectionStyle = .none
+            cell.textLabel?.text = "Cargando..."
+            return cell
+        }
     }
-    
+
     private func set(content: RedditPostViewModel, forCell cell: UITableViewCell) {
         cell.textLabel?.text = content.title
         cell.textLabel?.numberOfLines = 0
         cell.detailTextLabel?.attributedText = content.subtitle
     }
-    
-    @objc(tableView:didEndDisplayingCell:forRowAtIndexPath:) func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+
+    @objc(tableView:willDisplayCell:forRowAtIndexPath:)
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let section = PostListSection(rawValue: indexPath.section), section == .loadMore else { return }
+        update()
+    }
+
+    @objc(tableView:didEndDisplayingCell:forRowAtIndexPath:)
+    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         postList[indexPath.row].cancelThumbnailFetch()
     }
 }
 
 extension RedditPostListViewController : UITableViewDelegate {
-    
 }
 
