1b7f1f07d7120bf48f26cf0d4621a9ba037784e9
Nico Ameghino <nicolas.ameghino@gmail.com>
2016-11-10 23:23:03 -0300

    Adds test for mock listing

--- MUYRedditTests/MUYRedditTests.swift
+++ MUYRedditTests/MUYRedditTests.swift
@@ -19,8 +19,20 @@ class MUYRedditTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+
+
+    func testMockListing() {
+        let e = expectation(description: "testing mock listings")
+        let postsViewModel = RedditPostListViewModel(redditService: MockRedditAPI(), subreddit: "")
+        postsViewModel.fetch {
+            XCTAssert(postsViewModel.count == 20)
+            e.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+
+    }
     
-    func testListing() {
+    func testRealListing() {
         let e = expectation(description: "fetch hot posts from r/argentina :-)")
         
         let api = RedditAPI()
